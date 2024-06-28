aab46346fdc95c193498cea3321a503720264cf6
Sheenam7 <sheenamdubey78@gmail.com>
2024-06-28 22:58:54 +0530

    weighted Graph Implementation

---
+++ gitJava/Graph/weightedGraph.java
@@ -0,0 +1,45 @@
+package Graph;
+import java.util.*;
+
+public class weightedGraph {
+    static class Edge{
+        int src;
+        int des;
+        int wt;
+
+        public Edge(int s, int d, int w){
+            this.des = d;
+            this.src = s;
+            this.wt = w;
+        }
+    }
+
+    public static void createGraph(ArrayList <Edge> graph[]){
+        for(int i =0;i<graph.length;i++){
+            graph[i] = new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0, 2,2));
+
+        graph[1].add(new Edge(1, 2,10));
+        graph[1].add(new Edge(0, 3, 0));
+
+        graph[2].add(new Edge(2, 0,2));
+        graph[2].add(new Edge(2, 1,10));
+        graph[2].add(new Edge(2, 3,-1));
+
+        graph[3].add(new Edge(3, 1,0));
+        graph[3].add(new Edge(3, 2, -1));
+    }
+
+    public static void main(String args[]){
+        int V = 4;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+
+        for(int i = 0;i<graph[1].size();i++){
+            Edge e = graph[1].get(i);
+            System.out.println(e.des+" "+e.wt);
+        }
+    }
+    
+}
